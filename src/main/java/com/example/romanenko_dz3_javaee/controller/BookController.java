6869725bc9fa7ca94c40cb628954de554e7677a2
romashaa <mariia.romanenko@ukma.edu.ua>
2022-04-01 16:26:46 +0300

    before changing

--- src/main/java/com/example/romanenko_dz3_javaee/controller/BookController.java
+++ src/main/java/com/example/romanenko_dz3_javaee/controller/BookController.java
@@ -3,22 +3,45 @@ package com.example.romanenko_dz3_javaee.controller;
 import com.example.romanenko_dz3_javaee.dto.BookDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+//@RequestMapping("/books")
 public class BookController {
 
-    @GetMapping("/book-create")
-    public String bookForm(){
-        return "book-create";
-    }
+//    @GetMapping("/book-create")
+//    public String bookForm(){
+//        return "book-create";
+//    }
+//    @PostMapping("/book-create")
+//    public String saveBook(final BookDto bookDto, final Model model){
+//        System.out.println("save book: " + bookDto);
+//        model.addAttribute("book",bookDto);
+//        return "saved-book";
+//    }
+
+
+
+        List<BookDto> books = new ArrayList<>();
+
+        @GetMapping("/book-create")
+        public String bookForm(){
+            return "book-create";
+        }
+
+
+        @GetMapping("show_books")
+        public String getBooks(Model model) {
+            model.addAttribute("books", books);
+            return "saved-book";
+        }
+
     @PostMapping("/book-create")
-    public String saveBook(final BookDto bookDto, final Model model){
-        System.out.println("save book: " + bookDto);
-        model.addAttribute("book",bookDto);
-        return "saved-book";
+    public String createBook(@ModelAttribute BookDto book) {
+        books.add(book);
+        return "redirect:/show_books";
     }
 }
